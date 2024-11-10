0d139ef48142c53b45ed1761b2d163ab76eb190a
badgelatte <saucy5939@gmail.com>
2024-11-10 21:07:40 +0900

    물품 리스트를 보여주고 구매할 물품 입력 받는 과정까지 구현
    products.md를 읽어 store에 넣고 원하는 물품을 입력받는 과정까지 구현

--- src/main/java/store/Application.java
+++ src/main/java/store/Application.java
@@ -2,6 +2,14 @@ package store;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        OutputView outputView = new OutputView();
+        InputView inputView = new InputView();
+
+        Store store = new Store();
+        outputView.start(store);
+
+        ShoppingCart cart = new ShoppingCart();
+        inputView.readItem(cart);
+
     }
 }
