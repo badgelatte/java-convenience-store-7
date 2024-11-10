8d15351cfd54f501e125e98c7c276c982e307b8e
badgelatte <saucy5939@gmail.com>
2024-11-11 02:32:00 +0900

    java code 스타일 적용
    java code 스타일 적용이 안된 것을 확인하여 이를 적용함

--- src/main/java/store/InputView.java
+++ src/main/java/store/InputView.java
@@ -4,7 +4,7 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
     public void readItem(ShoppingCart cart) {
-        try{
+        try {
             System.out.println("구매하실 상품명과 수량을 입력해 주세요.(예: [사이다-2], [감자칩-1])");
             String item = Console.readLine();
             cart.addItems(item);
