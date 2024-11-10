eba81c5504bcdb1de1e97608f4985bcb781efabc
badgelatte <saucy5939@gmail.com>
2024-11-11 03:29:29 +0900

    재고 없음인가를 판단하는 기능 추가
    재고 없음이라 적힐 경우 0을 반환하고 숫자가 적힐 경우 그 숫자를 반한하는 메서드 구현

--- src/main/java/store/OutputView.java
+++ src/main/java/store/OutputView.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.List;
+import java.util.Objects;
 
 public class OutputView {
     private static final String PRODUCTS_FILE_PATH = "src/main/resources/products.md";
@@ -41,10 +42,18 @@ public class OutputView {
     public Product makeProduct(String input) {
         String[] inputs = input.split(",");
 
-        int price = Integer.parseInt(inputs[1]);
+        int price = validatePrice(inputs[1]);
         int quantity = Integer.parseInt(inputs[2]);
 
         return new Product(inputs[0], price, quantity, inputs[3]);
+    }
+
+    public int validatePrice(String input) {
+        int price = 0;
+        if(!Objects.equals(input, "재고 없음")) {
+            price = Integer.parseInt(input);
+        }
+        return price;
     }
 
     public void printProduct(Product product) {
