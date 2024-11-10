2bc6eb0aae08a315bd8ccb505b060d7719761dde
badgelatte <saucy5939@gmail.com>
2024-11-10 20:51:06 +0900

    구매 목록 입력을 바탕으로 shoppingCart의 map에 저장하는 기능 구현
    구매하고자하는 목록을 입력하면 그에 맞춰 이름, 갯수로 map에 저장하는 기능 구현

--- src/main/java/store/ShoppingCart.java
+++ src/main/java/store/ShoppingCart.java
@@ -5,4 +5,30 @@ import java.util.Map;
 
 public class ShoppingCart {
     Map<String, Integer> items = new HashMap<>();
+
+    public void addItems(String input) {
+        String[] itemList = input.split(",");
+        for (String item : itemList) {
+            String itemName = outputItemName(item);
+            int quantity = outputItemQuantity(item);
+            items.put(itemName, quantity);
+        }
+    }
+
+    public String outputItemName(String item) {
+        String itemName = item.split("-")[0];
+
+        return itemName.substring(1);
+    }
+
+    public int outputItemQuantity(String item) {
+        try {
+            item = item.split("-")[1];
+            item = item.substring(0, item.length() - 1);
+            return Integer.parseInt(item);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 올바르지 않은 형식으로 입력했습니다. 다시 입력해주세요");
+        }
+
+    }
 }
