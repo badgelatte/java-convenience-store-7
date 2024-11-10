3bd60d4733736983f0cd739ddb51b8c11781309d
badgelatte <saucy5939@gmail.com>
2024-11-10 20:57:39 +0900

    InputView 클래스 구현
    구매할 상품명과 수량 입력

---
+++ src/main/java/store/InputView.java
@@ -0,0 +1,16 @@
+package store;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    public void readItem(ShoppingCart cart) {
+        try{
+            System.out.println("구매하실 상품명과 수량을 입력해 주세요.(예: [사이다-2], [감자칩-1])");
+            String item = Console.readLine();
+            cart.addItems(item);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            readItem(cart);
+        }
+    }
+}
