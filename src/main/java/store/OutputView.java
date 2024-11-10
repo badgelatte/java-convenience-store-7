3fdec5639310f9dd38160f0c037bdd8a9ac4b681
badgelatte <saucy5939@gmail.com>
2024-11-10 21:02:37 +0900

    시작하는 메서드 start 추가
    시작 문구를 출력하는 메서드와 fileRead 메서드를 사용하는 메서드 구현

--- src/main/java/store/OutputView.java
+++ src/main/java/store/OutputView.java
@@ -8,6 +8,16 @@ import java.util.Objects;
 public class OutputView {
     private static final String PRODUCTS_FILE_PATH = "src/main/resources/products.md";
 
+    public void start(Store store) {
+        printWelcomeMsg();
+        fileRead(store);
+    }
+
+    public void printWelcomeMsg() {
+        System.out.println("안녕하세요. W편의점입니다.");
+        System.out.println("현재보유하고 있는 상품입니다. \n");
+    }
+
     public void fileRead(Store store) {
         try (BufferedReader reader = new BufferedReader(new FileReader(PRODUCTS_FILE_PATH))) {
             String line;
