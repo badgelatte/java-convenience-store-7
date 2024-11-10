a670f870fbe396de73b5ed777366f381712eff99
badgelatte <saucy5939@gmail.com>
2024-11-11 01:20:57 +0900

    fileRead 메서드 길이 줄임
    fileRead 메서드 길이가 10라인을 넘겨 이를 줄임

--- src/main/java/store/OutputView.java
+++ src/main/java/store/OutputView.java
@@ -3,13 +3,15 @@ package store;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Objects;
+import java.util.List;
 
 public class OutputView {
     private static final String PRODUCTS_FILE_PATH = "src/main/resources/products.md";
+
     public void start(Store store) {
         printWelcomeMsg();
-        fileRead(store);
+        List<String> productInfos = fileRead(PRODUCTS_FILE_PATH);
+        inputProductToStore(productInfos, store);
     }
 
     public void printWelcomeMsg() {
@@ -17,25 +19,21 @@ public class OutputView {
         System.out.println("현재보유하고 있는 상품입니다. \n");
     }
 
-    public void fileRead(Store store) {
-        try (BufferedReader reader = new BufferedReader(new FileReader(PRODUCTS_FILE_PATH))) {
-            String line;
-            while ((line = reader.readLine())!= null) {
-                inputProduct(line, store);
-            }
+    public List<String> fileRead(String path) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(path))) {
+            return reader.lines().skip(1).toList();
         } catch (IOException e) {
             throw new IllegalArgumentException("[ERROR] 파일을 찾을 수 없습니다.");
         }
     }
 
-    public void inputProduct(String input, Store store) {
-        if (input.contains("name")) {
-            return;
-        }
-        Product product = makeProduct(input);
+    public void inputProductToStore(List<String> productList, Store store) {
+        for (String productInfo : productList) {
+            Product product = makeProduct(productInfo);
 
-        printProuct(product);
-        store.addProduct(product);
+            printProduct(product);
+            store.addProduct(product);
+        }
     }
 
     public Product makeProduct(String input) {
@@ -47,7 +45,7 @@ public class OutputView {
         return new Product(inputs[0], price, quantity, inputs[3]);
     }
 
-    public void printProuct(Product product) {
+    public void printProduct(Product product) {
         System.out.println(product.toString());
     }
 }
