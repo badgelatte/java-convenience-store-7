a903dafacfba017164e0eb8d795a8c2327da37e4
badgelatte <saucy5939@gmail.com>
2024-11-11 19:20:30 +0900

    입력 받는 메서드를 OutputView에서 InputView로 이동
    giveAwayPromotionMsg, promotionOutOfStockMsg, membershipDiscountMsg 메서드는
    입력을 받는 메서드들이기 때문에 InputView에 있는것이 맞다 판단하여 이로 옮김

--- src/main/java/store/OutputView.java
+++ src/main/java/store/OutputView.java
@@ -82,20 +82,4 @@ public class OutputView {
     public void printProduct(Product product) {
         System.out.println(product.toString());
     }
-
-    public static String giveAwayPromotionMsg(String name, Promotion promotion) {
-        int promotionGet = promotion.getGet();
-        System.out.println("현재 " + name + "은(는) " + promotionGet + "개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)");
-        return Console.readLine();
-    }
-
-    public static String promotionOutOfStockMsg(String name, int quantity) {
-        System.out.println("현재 " + name + " " + quantity + "개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)");
-        return Console.readLine();
-    }
-
-    public static String membershipDiscountMsg() {
-        System.out.println("멤버십 할인을 받으시겠습니까? (Y/N)");
-        return Console.readLine();
-    }
 }
