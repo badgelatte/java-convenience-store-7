8ee5b15675bc6e5956f0aa66ec4fc72fa69dec9a
badgelatte <saucy5939@gmail.com>
2024-11-11 22:57:57 +0900

    추가 구매를 묻는 메서드 구현
    추가 구매를 묻는 메서드 구현

--- src/main/java/store/InputView.java
+++ src/main/java/store/InputView.java
@@ -6,8 +6,8 @@ public class InputView {
     public void readItem(ShoppingCart cart) {
         try {
             System.out.println("구매하실 상품명과 수량을 입력해 주세요.(예: [사이다-2], [감자칩-1])");
-            String item = Console.readLine();
-            cart.addItems(item);
+            String inputItem = Console.readLine();
+            cart.addItems(inputItem);
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
             readItem(cart);
@@ -27,6 +27,11 @@ public class InputView {
 
     public static String membershipDiscountMsg() {
         System.out.println("멤버십 할인을 받으시겠습니까? (Y/N)");
+        return Console.readLine();
+    }
+
+    public static String oneMorePurchase() {
+        System.out.println("감사합니다. 구매하고 싶은 다른 상품이 있나요? (Y/N)");
         return Console.readLine();
     }
 }
